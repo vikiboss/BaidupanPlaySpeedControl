6ce2b34df737faf21cfe9d4611357ce0703719f0
vikiboss <vikiboss@qq.com>
2020-06-16 20:48:58 +0800

    :zap: 优化交互 去除SVIP提示的div框

--- baidupan-playspeed-contron.js
+++ baidupan-playspeed-contron.js
@@ -1,14 +1,13 @@
 // ==UserScript==
-// @name         百度网盘视频倍速播放(2020/6/15更新）
+
+// @name         百度网盘视频倍速播放(2020/6/16更新）
 // @namespace    none
-// @version      1.0.0
+// @version      1.2.0
 // @description  简单的倍速播放脚本，小白作品，欢迎提意见
-// @homme        https://github.com/vikiboss/baidupan-playspeed-control
+// @github       https://github.com/vikiboss/baidupan-playspeed-control
 // @author       vikiboss (https://github.com/vikiboss)
 // @grant        none
 // @license      MIT
-// @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
-// @require      https://code.jquery.com/jquery-latest.js
 // @include      *://pan.baidu.com/disk/home*
 // @include      *://yun.baidu.com/disk/home*
 // @include      *://pan.baidu.com/s*
@@ -20,12 +19,19 @@
 (function () {
   'use strict';
   window.onload = function () {
+    
+    // === 按钮设置 ===
     var options = [
-      { str: '慢速', rate: 1.0 },
-      { str: '中速', rate: 1.5 },
-      { str: '快速', rate: 2 },
-      { str: '极速', rate: 3 },
+      { str: '慢速(x0.5)', rate: 0.5 },
+      { str: '较慢(x0.75)', rate: 0.75 },
+      { str: '标准(x1.0)', rate: 1.0 },
+      { str: '中速(x1.25)', rate: 1.25 },
+      { str: '快速(x1.5)', rate: 1.5 },
+      { str: '极速(x2.0)', rate: 2.0 },
     ];
+
+    
+    // === 生成按钮函数 ===
     var generateBtn = function (rateStr, rate) {
       var btn = document.createElement('a');
       btn.className = 'g-button';
@@ -42,6 +48,12 @@
       btn.innerHTML = btnInnerHtml;
       return btn;
     };
+    
+    // === 去除vip的div框 ===
+    var vip = document.getElementsByClassName('privilege-box')[0];
+    vip.style.display = 'none';
+    
+    // === 插入倍速按钮 ===
     var div = document.getElementsByClassName('video-toolbar-buttonbox')[0];
     var btns = options.map(function (option) {
       return generateBtn(option.str, option.rate);
